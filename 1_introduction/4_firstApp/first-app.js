4bb5d71cdf5c5cd0948e2887a4be1b2b42bc9c69
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2024-04-24 18:15:55 +0200

    1. installation nodeJS notes

--- 1_introduction/4_firstApp/first-app.js
+++ 1_introduction/4_firstApp/first-app.js
@@ -1,2 +1,8 @@
 import fs from "fs";
 fs.writeFileSync("hello.txt", "Hello from nodeJS!");
+
+/* NOTE:
+On Linux, 
+https://nodejs.org/en/download/package-manager/all#debian-and-ubuntu-based-linux-distributions
+use n or brew
+*/
