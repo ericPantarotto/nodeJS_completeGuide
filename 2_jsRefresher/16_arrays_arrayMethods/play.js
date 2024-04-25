c0aa884f3242794a8b9ad65e349620f616f043eb
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2024-04-25 14:58:48 +0200

    2. arrays & arrays methods

---
+++ 2_jsRefresher/16_arrays_arrayMethods/play.js
@@ -0,0 +1,17 @@
+const person = {
+  name: 'Max',
+  age: 29,
+  greet() {
+    console.log('Hi, I am ' + this.name);
+  }
+};
+
+const hobbies = ['Sports', 'Cooking'];
+// for (let hobby of hobbies) {
+//     console.log(hobby);
+// }
+console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
+console.log(hobbies);
+
+// NOTE: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array#
+// INFO: map returns a new array, taking a function applied on each item of the array
