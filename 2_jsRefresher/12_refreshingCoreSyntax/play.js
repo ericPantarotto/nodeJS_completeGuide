807236f074290c7146e395fbff0adec077df53aa
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2024-04-25 12:07:48 +0200

    2. refreshing the core syntax

---
+++ 2_jsRefresher/12_refreshingCoreSyntax/play.js
@@ -0,0 +1,16 @@
+const firstName = 'Max';
+const  age = 29;
+const hasHobbies = true;
+
+function summarizeUser(userName, userAge, userHasHobby) {
+  return (
+    'Name is ' +
+    userName +
+    ', age is ' +
+    userAge +
+    ' and the user has hobbies: ' +
+    userHasHobby
+  );
+}
+
+console.log(summarizeUser(firstName, age, hasHobbies));
