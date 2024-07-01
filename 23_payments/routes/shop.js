8ad96d9ebfd731036db987916e7a050410601e4b
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2024-07-02 00:44:23 +0200

    23. module set-up

---
+++ 23_payments/routes/shop.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import shopController from '../controllers/shop.js';
+import authMiddleware from '../middlewares/is-auth.js';
+
+const router = express.Router();
+
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/products/:productId', shopController.getProduct);
+
+router.get('/cart', authMiddleware.isAuthenticated, shopController.getCart);
+router.post('/cart', authMiddleware.isAuthenticated, shopController.postCart);
+
+router.post(
+  '/cart-delete-item',
+  authMiddleware.isAuthenticated,
+  shopController.postCartDeleteProduct
+);
+
+router.get('/orders', authMiddleware.isAuthenticated, shopController.getOrders);
+router.post(
+  '/create-order',
+  authMiddleware.isAuthenticated,
+  shopController.postOrder
+);
+
+router.get('/orders/:orderId', authMiddleware.isAuthenticated, shopController.getInvoice); 
+// router.get('/checkout', shopController.getCheckout);
+
+export const expRouter = router;
