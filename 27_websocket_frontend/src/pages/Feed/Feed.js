63c7d7e4b94cf67e198daebe8659ea8a3498e473
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2024-07-14 23:06:53 +0200

    27. Identifying Realtime Potential

--- 27_websocket_frontend/src/pages/Feed/Feed.js
+++ 27_websocket_frontend/src/pages/Feed/Feed.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react';
-import openSocket from "socket.io-client";
+import openSocket from 'socket.io-client';
 
 import Button from '../../components/Button/Button';
 import ErrorHandler from '../../components/ErrorHandler/ErrorHandler';
@@ -54,6 +54,22 @@ class Feed extends Component {
     console.log(serverHost);
     openSocket(serverHost);
   }
+
+  addPost = post => {
+    this.setState(prevState => {
+      const updatedPosts = [...prevState.posts];
+      if (prevState.postPage === 1) {
+        if (prevState.posts.length >= 2) {
+          updatedPosts.pop();
+        }
+        updatedPosts.unshift(post);
+      }
+      return {
+        posts: updatedPosts,
+        totalPosts: prevState.totalPosts + 1
+      };
+    });
+  };
 
   loadPosts = direction => {
     if (direction) {
