8d7b405935f7a4b344512cb3a05df79183774343
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2024-07-17 21:34:52 +0200

    28. Adding Posts & Pagination

--- 28_graphql_frontend/src/pages/Feed/Feed.js
+++ 28_graphql_frontend/src/pages/Feed/Feed.js
@@ -268,13 +268,13 @@ class Feed extends Component {
               p => p._id === prevState.editPost._id
             );
             updatedPosts[postIndex] = post;
-          } else if (prevState.posts.length < 2) {
-            updatedPosts = prevState.posts.concat(post);
+          } else {
+            if (prevState.posts.length >= 2) {
+              updatedPosts.pop();
+            }
+            updatedPosts.unshift(post);
           }
-          // } else {
-          //   updatedPosts.pop();
-          //   updatedPosts.unshift(post);
-          // }
+         
           return {
             posts: updatedPosts,
             isEditing: false,
