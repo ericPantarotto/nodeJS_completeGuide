ad34d61ea867fe310ee5b0996edba2d41ef90e70
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2024-08-01 11:03:53 +0200

    33. Finishing the Rest Routes

--- 33_typescript_node/routes/todo.ts
+++ 33_typescript_node/routes/todo.ts
@@ -2,19 +2,36 @@ import { Router } from 'express';
 
 import { Todo } from '../models/todo';
 
-const toDos: Array<Todo> = [];
+let toDos: Array<Todo> = [];
 const router = Router();
 
 router.get('/', (req, res, next) => {
-  res.status(200).json({ todos: toDos });
+  res.status(200).json({ toDos: toDos });
 });
 
-router.post('/', (req, res, next) => {
+router.post('/todo', (req, res, next) => {
   const newToDo: Todo = {
     id: new Date().toISOString(),
     text: req.body.text,
   };
 
   toDos.push(newToDo);
+  res.status(201).json({ message: 'Added Todo', toDo: newToDo, toDos: toDos });
 });
+
+router.put('/todo:todoId', (req, res, next) => {
+  const tId = req.params.todoId;
+  const todoIndex = toDos.findIndex(item => item.id === tId);
+  if (todoIndex >= 0) {
+    toDos[todoIndex] = { id: tId, text: req.body.text };
+    return res.status(200).json({ message: 'Updated todo', toDos: toDos });
+  }
+  res.status(404).json({ message: "Couldn't find todo for this id" });
+});
+
+router.delete('/todo:todoId', (req, res, next) => {
+ toDos.filter(item => item.id !== req.params.todoId) 
+  res.status(200).json({ message: 'Deleted todo', toDos: toDos });
+});
+
 export default router;
